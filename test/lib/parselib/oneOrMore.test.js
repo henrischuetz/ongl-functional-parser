69487b445436055b2114bf167c204d0c015242e5
Fubar <henri_schuetz@hotmail.com>
2024-03-17 18:00:42 +0100

    added tests for one or more parser

---
+++ test/lib/parselib/oneOrMore.test.js
@@ -0,0 +1,27 @@
+const { parse, oneOrMore, resultSet } = require('../../../lib/parselib')
+
+const aParseAll = oneOrMore(parse('a'))
+
+test('make a one or more a parser', ()=>{
+    expect(oneOrMore(parse('a'))).toBeInstanceOf(Function)
+})
+
+test('parse a', ()=>{
+    expect(aParseAll('a')).toStrictEqual(resultSet('a+', 'a', ''))
+})
+
+test('prase no char', ()=>{
+    expect(aParseAll('xy')).toStrictEqual(resultSet('a+', '', 'xy'))
+})
+
+test('parse only first char of ab', ()=>{
+    expect(aParseAll('ab')).toStrictEqual(resultSet('a+', 'a', 'b'))
+})
+
+test('parse multiple a\'s from aaab', ()=>{
+    expect(aParseAll('aaab')).toStrictEqual(resultSet('a+', 'aaa', 'b'))
+})
+
+test('parse empty string', ()=>{
+    expect(aParseAll('')).toStrictEqual(resultSet('a+', '', ''))
+})
